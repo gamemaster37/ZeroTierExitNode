3c432184c944458d393c015fe1ee69e348ccde01
gamemaster37 <mendinuetaj@uninorte.edu.co>
2024-07-21 19:58:18 -0500

    Add files via upload

---
+++ dockerfile
@@ -0,0 +1,12 @@
+FROM ubuntu:latest
+
+EXPOSE 9993
+
+RUN apt-get update && apt-get upgrade -y 
+RUN apt-get install iptables -y && apt-get install iproute2 -y && apt-get install curl -y && apt-get install iptables-persistent -y
+
+COPY first_run.sh /bin/first_run.sh
+RUN chmod +x /bin/first_run.sh
+
+ENTRYPOINT ["/bin/bash","/bin/first_run.sh"]
+
---
+++ first_run.sh
@@ -0,0 +1,30 @@
+# first_run.sh
+#!/bin/bash
+flag_path=/data
+flag_file=$flag_path/first_run
+
+echo "Running setup script"
+
+if [ ! -f "$flag_file" ]; then    
+    # Your script logic here
+    curl https://install.zerotier.com/ | bash 
+
+    sleep 2 && zerotier-cli join $1
+    
+    echo 'net.ipv4.ip_forward=1' >> /etc/sysctl.conf
+    export ZT_IF=$(ip link show |  grep -Po '^\d+: \K[^:]+' | grep zt) 
+    export WAN_IF=$(ip link show |  grep -Po '^\d+: \K[^:]+' | grep eth) 
+
+    iptables -t nat -A POSTROUTING -o $WAN_IF -j MASQUERADE
+    iptables -A FORWARD -m conntrack --ctstate RELATED,ESTABLISHED -j ACCEPT
+    iptables -A FORWARD -i $ZT_IF -o $WAN_IF -j ACCEPT
+
+    netfilter-persistent save
+    iptables-save && iptables-legacy-save
+    mkdir -p "$flag_path" && touch "$flag_file"
+fi
+
+zerotier-one -d
+sleep 5 && zerotier-cli listnetworks
+echo "Sleeping infinitely"
+sleep infinity
