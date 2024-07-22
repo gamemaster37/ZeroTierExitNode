ecf52cfb874d869a5a4ba9edd421d384d1ac7880
gamemaster37 <mendinuetaj@uninorte.edu.co>
2024-07-21 20:53:18 -0500

    cleaning run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#docker run -it --rm --cap-add NET_ADMIN --cap-add SYS_ADMIN --network host --device /dev/net/tun ubuntu:latest /bin/bash
 docker run -it --rm --cap-add NET_ADMIN --cap-add SYS_ADMIN --network host --device /dev/net/tun zt-exit-node:latest $NET_ID
